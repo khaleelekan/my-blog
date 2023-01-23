5e14b4c879dc0d278213e9ff514b5bdf71c46be3
unknown <kalilulekan@gmail.com>
2023-01-23 21:49:52 +0100

    filter function

--- script.js
+++ script.js
@@ -1,61 +1,80 @@
- const postsContainer = document.getElementById('posts-container');
- const loader = document.querySelector('.loader');
+const postsContainer = document.getElementById('posts-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
 
- let limit = 5;
- let page = 1;
+let limit = 5;
+let page = 1;
 
- //fetch post from api
- async function getPosts(){
-    const res = await fetch(
-        `https://jsonplaceholder.typicode.com/posts?_limits=${limit}&_page=${page}`
-        );
+// Fetch posts from API
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
 
-        const data = await res.json();
+  const data = await res.json();
 
-        return data;
- } 
+  return data;
+}
 
- //show post in dom
- async function showPosts() {
-    const posts = await getPosts();
+// Show posts in DOM
+async function showPosts() {
+  const posts = await getPosts();
 
-    posts.forEach(post => {
-        const postEl = document.createElement('div');
-        postEl.classList.add('post');
-        postEl.innerHTML = ` 
-         <div class='number'>${post.id}</div>
-         <div class='post-info'>
-         <h2 class='post-title'>${post.title}</h2>
-         <p class='post-body'>${post.body}</p>
-         </div>`
+  posts.forEach(post => {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `;
 
-         postsContainer.appendChild(postEl);
-    });
+    postsContainer.appendChild(postEl);
+  });
+}
 
-   
+// Show loader & fetch more posts
+function showLoading() {
+  loading.classList.add('show');
 
-   
- }
- //show loading
- function showLoading(){
-   showLoading.classList.add('show');
+  setTimeout(() => {
+    loading.classList.remove('show');
 
-   setTimeout(()=>{
-      showLoading.classList.remove('show')
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
 
-      setTimeout(()=>{
-         page++;
-         showPosts()
-      })
-   },1000)
- }
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
 
- //show posts
- showPosts();
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
 
- window.addEventListener('scroll', () => {
-   const {scrollHeight, scrollTop, clientHeight} = document.documentElement;
-   if (scrollTop + clientHeight >= scrollHeight){
-      showLoading()
-   }
- })
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
+// Show initial posts
+showPosts();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+filter.addEventListener('input', filterPosts);
